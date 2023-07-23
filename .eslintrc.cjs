f27aee92a07a0b2e2de9644e9751fc244326f470
Cosmin Tanislav <demonsingur@gmail.com>
2023-07-24 01:35:42 +0300

    Improve linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,62 +3,67 @@ module.exports = {
     env: {
         node: true,
     },
-    parser: "@typescript-eslint/parser",
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    parserOptions: { ecmaVersion: 2018, sourceType: "module" },
-    plugins: ["@typescript-eslint"],
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: 'tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
     rules: {
-        "linebreak-style": ["error", "unix"],
+        'linebreak-style': ['error', 'unix'],
 
-        indent: "off",
-        "@typescript-eslint/indent": "off",
+        indent: 'off',
+        '@typescript-eslint/indent': 'off',
 
-        "comma-dangle": "off",
-        "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+        'comma-dangle': 'off',
+        '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
 
-        quotes: "off",
-        "@typescript-eslint/quotes": ["error", "single"],
+        quotes: 'off',
+        '@typescript-eslint/quotes': ['error', 'single'],
 
-        semi: "off",
-        "@typescript-eslint/semi": ["error", "always"],
+        semi: 'off',
+        '@typescript-eslint/semi': ['error', 'always'],
 
-        "no-extra-semi": "off",
-        "@typescript-eslint/no-extra-semi": ["error"],
+        'no-extra-semi': 'off',
+        '@typescript-eslint/no-extra-semi': ['error'],
 
-        "space-before-function-paren": "off",
-        "@typescript-eslint/space-before-function-paren": [
-            "error",
+        'space-before-function-paren': 'off',
+        '@typescript-eslint/space-before-function-paren': [
+            'error',
             {
-                anonymous: "always",
-                asyncArrow: "always",
-                named: "never",
+                anonymous: 'always',
+                asyncArrow: 'always',
+                named: 'never',
             },
         ],
 
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "error",
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
             {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
             },
         ],
 
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-empty-interface": "off",
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
 
-        "@typescript-eslint/strict-boolean-expressions": [
-            "error",
+        '@typescript-eslint/strict-boolean-expressions': [
+            'error',
             {
                 allowNullableBoolean: true,
             },
         ],
 
-        "vue/no-unused-components": ["warn"],
-        "vue/comment-directive": "off",
-
-        "no-constant-condition": [
-            "error",
+        'no-constant-condition': [
+            'error',
             {
                 checkLoops: false,
             },
