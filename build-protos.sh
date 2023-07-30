4e6f5093a9d95df58abb3b3be84b6c01468a53a1
Cosmin Tanislav <demonsingur@gmail.com>
2023-07-31 00:20:08 +0300

    Use bigint for protos

--- build-protos.sh
+++ build-protos.sh
@@ -1,5 +1,6 @@
 protoc --proto_path='./src/proto/protos' \
        --plugin='./node_modules/.bin/protoc-gen-ts_proto' \
        --ts_proto_opt=esModuleInterop=true \
+       --ts_proto_opt=forceLong=bigint \
        --ts_proto_out='./src/proto/types' \
        ./src/proto/protos/*
