938073e01333685e91f1f0c44d6dc6790b2e247e
Cosmin Tanislav <demonsingur@gmail.com>
2023-07-31 00:19:55 +0300

    Move proto building to separate script

---
+++ server/build-protos.sh
@@ -0,0 +1,5 @@
+protoc --proto_path='./src/proto/protos' \
+       --plugin='./node_modules/.bin/protoc-gen-ts_proto' \
+       --ts_proto_opt=esModuleInterop=true \
+       --ts_proto_out='./src/proto/types' \
+       ./src/proto/protos/*
