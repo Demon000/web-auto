e53239d012b1007eed1c9547460f5252be35e396
Cosmin Tanislav <demonsingur@gmail.com>
2023-08-17 00:36:25 +0300

    WIP

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,7 +12,6 @@ import HelloWorld from './components/HelloWorld.vue';
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
