197a2da0d857e5a8b27c64ff81eff46dca4620a1
Cosmin Tanislav <demonsingur@gmail.com>
2023-08-24 21:21:09 +0300

    electron-ipc-renderer: check window if methods are exposed

--- electron-ipc-renderer/src/index.ts
+++ electron-ipc-renderer/src/index.ts
@@ -8,11 +8,21 @@ export class RendererCommuncationChannel<
     L extends MethodsMap,
     R extends MethodsMap,
 > extends CommunicationChannel<L, R> {
+    public checkWindow(name: string, eventName: string): void {
+        if (name in window) {
+            return;
+        }
+
+        throw new Error(
+            `Channel ${this.name}, event ${eventName} not exposed on window`,
+        );
+    }
     public send<K extends keyof R & string>(
         eventName: K,
         ...args: Parameters<R[K]>
     ): void {
         const name = composeName(this.name, eventName);
+        this.checkWindow(name, eventName);
         (window as Record<string, any>)[name].fn(...args);
     }
 
@@ -21,11 +31,13 @@ export class RendererCommuncationChannel<
         ...args: Parameters<R[K]>
     ): Promise<Awaited<ReturnType<R[K]>>> {
         const name = composeName(this.name, eventName);
+        this.checkWindow(name, eventName);
         return (window as Record<string, any>)[name].fn(...args);
     }
 
     public on<K extends keyof L & string>(eventName: K, callback: L[K]): void {
         const name = composeName(this.name, eventName);
+        this.checkWindow(name, eventName);
         (window as Record<string, any>)[name].fn(callback);
     }
 }
