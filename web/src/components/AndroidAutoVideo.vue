356df8d0256d275e78f2cb3966317280743ee016
Cosmin Tanislav <demonsingur@gmail.com>
2023-08-27 00:44:27 +0300

    WIP

--- web/src/components/AndroidAutoVideo.vue
+++ web/src/components/AndroidAutoVideo.vue
@@ -8,46 +8,52 @@ import { AndroidAutoMainMethod } from '@web-auto/electron-ipc-android-auto';
 import { AndroidAutoRendererMethod } from '@web-auto/electron-ipc-android-auto';
 import { onMounted, ref, type Ref } from 'vue';
 import { transformFittedPoint } from 'object-fit-math';
+import { type FitMode } from 'object-fit-math/dist/types';
 import { TouchAction } from '@web-auto/android-auto-proto';
 
 const canvasRef: Ref<HTMLCanvasElement | undefined> = ref(undefined);
-let canvasRealSize: { width: number; height: number } = { width: 0, height: 0 };
 let canvasSize: { width: number; height: number } = { width: 0, height: 0 };
+let canvasRealSize: { width: number; height: number } = { width: 0, height: 0 };
+let canvasObjectFit: FitMode = 'contain';
+let canvasObjectPosition: [string, string] = ['0', '0'];
 
-const setCanvasSizes = () => {
+function assert(conditional: boolean, message?: string): asserts conditional {
+    if (!conditional) throw new Error(message);
+}
+
+const setCanvasSize = () => {
     const canvas = canvasRef.value;
-    if (canvas === undefined) {
-        return;
-    }
-
-    canvasRealSize.width = canvas.width;
-    canvasRealSize.height = canvas.height;
+    assert(canvas !== undefined);
 
     const canvasBoundingBox = canvas.getBoundingClientRect();
     canvasSize.width = canvasBoundingBox.width;
     canvasSize.height = canvasBoundingBox.height;
 };
 
+const setCanvasRealSize = () => {
+    const canvas = canvasRef.value;
+    assert(canvas !== undefined);
+
+    canvasRealSize.width = canvas.width;
+    canvasRealSize.height = canvas.height;
+};
+
 onMounted(() => {
     const canvas = canvasRef.value;
-    if (canvas === undefined) {
-        return;
-    }
+    assert(canvas !== undefined);
 
     const context = canvas.getContext('2d');
-    if (context === null) {
-        return;
-    }
+    assert(context !== null);
 
-    const resizeObserver = new ResizeObserver(setCanvasSizes);
-    resizeObserver.observe(canvas);
+    new ResizeObserver(setCanvasSize).observe(canvas);
 
     const decoder = new H264WebCodecsDecoder(canvas);
 
     decoder.emitter.on(H264WebCodecsDecoderEvent.DIMENSIONS, (data) => {
         canvas.width = data.width;
         canvas.height = data.height;
-        setCanvasSizes();
+
+        setCanvasRealSize();
     });
 
     decoder.emitter.on(H264WebCodecsDecoderEvent.FRAME, (data) => {
@@ -63,22 +69,18 @@ onMounted(() => {
 
 const translateCanvasPosition = (x: number, y: number): [number, number] => {
     const canvas = canvasRef.value;
-    if (canvas === undefined) {
-        return [0, 0];
-    }
+    assert(canvas !== undefined);
 
-    const { objectFit, objectPosition } = getComputedStyle(canvas);
-    const [left, top] = objectPosition.split(' ');
-    const { x: translatedX, y: translatedY } = transformFittedPoint(
+    const translatedPoint = transformFittedPoint(
         { x, y },
         canvasSize,
-        canvas,
-        objectFit as any,
-        left,
-        top,
+        canvasRealSize,
+        canvasObjectFit,
+        canvasObjectPosition[0],
+        canvasObjectPosition[1],
     );
 
-    return [Math.round(translatedX), Math.round(translatedY)];
+    return [Math.round(translatedPoint.x), Math.round(translatedPoint.y)];
 };
 
 const onCanvasClick = (event: MouseEvent) => {
@@ -122,7 +124,7 @@ canvas {
     width: 100%;
     height: 100%;
     display: block;
-    object-fit: contain;
+    object-fit: v-bind('canvasObjectFit');
     background: #000;
 }
 </style>
