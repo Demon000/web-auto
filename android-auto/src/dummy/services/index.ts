4604fdca383bd749343c9ebce5be0e541f2fa94e
Cosmin Tanislav <demonsingur@gmail.com>
2023-09-18 02:45:24 +0300

    android-auto: remove dummy service factory and crypto

--- android-auto/src/dummy/services/index.ts
+++ android-auto/src/dummy/services/index.ts
@@ -3,6 +3,5 @@ export * from './DummyAudioOutputService';
 export * from './DummyInputService';
 export * from './DummyMediaStatusService';
 export * from './DummyNavigationService';
-export * from './DummyServiceFactory';
 export * from './DummyVideoService';
 export * from './DummySensorService';
