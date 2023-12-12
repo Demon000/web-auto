426209f855c983e9baeb96c54f8863aa9d397e37
Cosmin Tanislav <demonsingur@gmail.com>
2023-12-12 13:07:34 +0200

    web-auto: add support for H265

--- electron/src/services/ElectronAndroidAutoVideoService.ts
+++ electron/src/services/ElectronAndroidAutoVideoService.ts
@@ -28,6 +28,8 @@ import type {
 import {
     annexBSplitNalu,
     h264ParseConfiguration,
+    h265ParseConfiguration,
+    h265ParseNaluHeader,
 } from '@yume-chan/scrcpy';
 
 enum CodecState {
@@ -40,11 +42,32 @@ enum CodecState {
 const toHex = (value: number) =>
     value.toString(16).padStart(2, '0').toUpperCase();
 
+const toUint32Le = (data: Uint8Array, offset: number) => {
+    return (
+        data[offset]! |
+        (data[offset + 1]! << 8) |
+        (data[offset + 2]! << 16) |
+        (data[offset + 3]! << 24)
+    );
+};
+
 const h264HasKeyFrame = (buffer: Uint8Array) => {
     for (const nalu of annexBSplitNalu(buffer)) {
         const naluType = nalu[0]! & 0x1f;
 
         if (naluType === 5) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const h265HasKeyFrame = (buffer: Uint8Array) => {
+    for (const nalu of annexBSplitNalu(buffer)) {
+        const header = h265ParseNaluHeader(nalu);
+
+        if (header.nal_unit_type === 19 || header.nal_unit_type === 20) {
             return true;
         }
     }
@@ -151,6 +174,43 @@ export class ElectronAndroidAutoVideoService extends VideoService {
         };
     }
 
+    protected parseH265CodecConfig(buffer: DataBuffer): VideoCodecConfig {
+        const {
+            generalProfileSpace,
+            generalProfileIndex,
+            generalProfileCompatibilitySet,
+            generalTierFlag,
+            generalLevelIndex,
+            generalConstraintSet,
+            cropLeft,
+            cropRight,
+            cropTop,
+            cropBottom,
+            croppedWidth,
+            croppedHeight,
+        } = h265ParseConfiguration(buffer.data);
+
+        const codec = [
+            'hev1',
+            ['', 'A', 'B', 'C'][generalProfileSpace]! +
+                generalProfileIndex.toString(),
+            toUint32Le(generalProfileCompatibilitySet, 0).toString(16),
+            (generalTierFlag ? 'H' : 'L') + generalLevelIndex.toString(),
+            toUint32Le(generalConstraintSet, 0).toString(16).toUpperCase(),
+            toUint32Le(generalConstraintSet, 4).toString(16).toUpperCase(),
+        ].join('.');
+
+        return {
+            codec,
+            cropLeft,
+            cropRight,
+            cropTop,
+            cropBottom,
+            width: croppedWidth,
+            height: croppedHeight,
+        };
+    }
+
     protected handleH264(buffer: DataBuffer): void {
         if (this.codecState === CodecState.STARTED) {
             this.ipcHandler.data(buffer.data);
@@ -192,6 +252,48 @@ export class ElectronAndroidAutoVideoService extends VideoService {
         }
     }
 
+    protected handleH265(buffer: DataBuffer): void {
+        if (this.codecState === CodecState.STARTED) {
+            this.ipcHandler.data(buffer.data);
+        } else if (this.codecState === CodecState.WAITING_FOR_CONFIG) {
+            assert(this.codecBuffer === undefined);
+
+            let config;
+            try {
+                config = this.parseH265CodecConfig(buffer);
+                this.logger.info('Parsed H265 config', config);
+            } catch (err) {
+                this.logger.error('Failed to parse H265 config', err);
+                return;
+            }
+
+            this.ipcHandler.codecConfig(config);
+
+            this.codecBuffer = buffer;
+            this.codecState = CodecState.WAITING_FOR_FIRST_FRAME;
+        } else if (this.codecState === CodecState.WAITING_FOR_FIRST_FRAME) {
+            assert(this.codecBuffer !== undefined);
+
+            if (!h265HasKeyFrame(buffer.data)) {
+                this.logger.error('Failed to find H265 keyframe');
+                return;
+            }
+
+            this.codecBuffer.appendBuffer(buffer);
+
+            this.ipcHandler.firstFrame(this.codecBuffer.data);
+
+            this.codecBuffer = undefined;
+            this.codecState = CodecState.STARTED;
+        } else {
+            this.logger.error(
+                `Cannot receive data in codec state ${
+                    CodecState[this.codecState]
+                }`,
+            );
+        }
+    }
+
     protected async handleData(
         buffer: DataBuffer,
         _timestamp?: bigint,
@@ -205,6 +307,9 @@ export class ElectronAndroidAutoVideoService extends VideoService {
         switch (videoCodecType) {
             case MediaCodecType.MEDIA_CODEC_VIDEO_H264_BP:
                 this.handleH264(buffer);
+                break;
+            case MediaCodecType.MEDIA_CODEC_VIDEO_H265:
+                this.handleH265(buffer);
                 break;
             default:
                 this.logger.error(
