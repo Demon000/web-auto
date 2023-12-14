0374ca6a28287ef4035027532cea55ec0bb97aa5
Cosmin Tanislav <demonsingur@gmail.com>
2023-12-15 00:17:40 +0200

    web-auto: build web as ESNext

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,14 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        target: 'esnext',
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'esnext',
+        },
+    },
     plugins: [
         vue({
             template: {
