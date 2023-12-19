6e46bcfc77ea1869cab4662a2175be78c70ec02a
Cosmin Tanislav <demonsingur@gmail.com>
2023-12-19 04:09:16 +0200

    web-auto: open browser by default

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        open: true,
+    },
     build: {
         target: 'esnext',
     },
