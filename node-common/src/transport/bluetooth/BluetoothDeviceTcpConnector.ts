8deacfac64e89e5dd4ac6420c2978c88acfd4163
Cosmin Tanislav <demonsingur@gmail.com>
2023-12-23 18:29:39 +0200

    web-auto: remove last eventemitter usage

--- node-common/src/transport/bluetooth/BluetoothDeviceTcpConnector.ts
+++ node-common/src/transport/bluetooth/BluetoothDeviceTcpConnector.ts
@@ -1,20 +1,8 @@
-import { EventEmitter } from 'eventemitter3';
 import { Duplex } from 'node:stream';
 import { LoggerWrapper, getLogger } from '@web-auto/logging';
-import { Server, Socket } from 'node:net';
-
-enum InternalEvent {
-    CONNECTION_SUCCESS,
-    CONNECTION_FAIL,
-}
-
-interface InternalEvents {
-    [InternalEvent.CONNECTION_SUCCESS]: (socket: Duplex) => void;
-    [InternalEvent.CONNECTION_FAIL]: (err: Error) => void;
-}
+import { Server } from 'node:net';
 
 export class BluetoothDeviceTcpConnector {
-    private internalEmitter = new EventEmitter<InternalEvents>();
     protected logger: LoggerWrapper;
 
     public constructor(
@@ -22,55 +10,30 @@ export class BluetoothDeviceTcpConnector {
         private name: string,
     ) {
         this.logger = getLogger(`${this.constructor.name}@${this.name}`);
-
-        this.onConnect = this.onConnect.bind(this);
     }
 
-    public onConnect(socket: Socket): void {
-        this.logger.debug('TCP socket connected');
+    private async connect(abortSignal: AbortSignal): Promise<Duplex> {
+        return new Promise<Duplex>((resolve, reject) => {
+            const onAbort = () => {
+                this.logger.info('Aborted wait for TCP connection');
+                this.tcpServer.removeListener('connection', onConnect);
+                reject(new Error('Aborted'));
+            };
 
-        this.internalEmitter.emit(InternalEvent.CONNECTION_SUCCESS, socket);
+            const onConnect = (socket: Duplex) => {
+                this.logger.info('Received TCP connection');
+                abortSignal.removeEventListener('abort', onAbort);
+                resolve(socket);
+            };
+
+            abortSignal.addEventListener('abort', onAbort);
+            this.tcpServer.once('connection', onConnect);
+        });
     }
 
     public async connectWithTimeout(timeoutMs: number): Promise<Duplex> {
-        const timeout = setTimeout(() => {
-            this.internalEmitter.emit(
-                InternalEvent.CONNECTION_FAIL,
-                new Error('Timed out'),
-            );
-        }, timeoutMs);
+        const abortSignal = AbortSignal.timeout(timeoutMs);
 
-        const cleanup = () => {
-            this.internalEmitter.removeAllListeners();
-            clearTimeout(timeout);
-            // eslint-disable-next-line @typescript-eslint/unbound-method
-            this.tcpServer.off('connection', this.onConnect);
-        };
-
-        return new Promise<Duplex>((resolve, reject) => {
-            this.internalEmitter.once(InternalEvent.CONNECTION_FAIL, (err) => {
-                this.internalEmitter.removeAllListeners();
-                reject(err);
-            });
-
-            this.internalEmitter.once(
-                InternalEvent.CONNECTION_SUCCESS,
-                (socket) => {
-                    this.internalEmitter.removeAllListeners();
-                    resolve(socket);
-                },
-            );
-
-            // eslint-disable-next-line @typescript-eslint/unbound-method
-            this.tcpServer.once('connection', this.onConnect);
-        })
-            .then((socket) => {
-                cleanup();
-                return socket;
-            })
-            .catch((err) => {
-                cleanup();
-                throw err;
-            });
+        return this.connect(abortSignal);
     }
 }
