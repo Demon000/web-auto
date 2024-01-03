4a527d658ffb6678dc922df05f1be5e5250f921a
Cosmin Tanislav <demonsingur@gmail.com>
2024-01-03 23:12:37 +0200

    web-auto: remove leftover TODO

--- eslint-config/index.cjs
+++ eslint-config/index.cjs
@@ -43,7 +43,6 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
 
-        // TODO
         '@typescript-eslint/strict-boolean-expressions': [
             'error',
             {
