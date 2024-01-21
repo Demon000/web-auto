9d19760fb2db99f627a3882941281fce00622f20
Cosmin Tanislav <demonsingur@gmail.com>
2024-01-22 00:54:55 +0200

    web-auto: create UsbTransport to use with UsbDevice

--- node-common/src/transport/usb/UsbDevice.ts
+++ node-common/src/transport/usb/UsbDevice.ts
@@ -5,9 +5,8 @@ import {
     Transport,
     type TransportEvents,
 } from '@web-auto/android-auto';
-import { UsbDuplex } from './UsbDuplex.js';
-import { DuplexTransport } from '../DuplexTransport.js';
 import type { UsbDeviceWrapper } from './UsbDeviceWrapper.js';
+import { UsbTransport } from './UsbTransport.js';
 
 export class UsbDevice extends Device {
     public constructor(
@@ -20,11 +19,7 @@ export class UsbDevice extends Device {
     public async connectImpl(events: TransportEvents): Promise<Transport> {
         await this.device.open();
 
-        const duplex = new UsbDuplex(this.device);
-        await duplex.claimInterface();
-        const transport = new DuplexTransport(duplex, events);
-
-        return transport;
+        return new UsbTransport(this.device, events);
     }
 
     protected override async handleDisconnect(reason: string): Promise<void> {
---
+++ node-common/src/transport/usb/UsbTransport.ts
@@ -0,0 +1,73 @@
+import {
+    Transport,
+    type TransportEvents,
+    TransportState,
+} from '@web-auto/android-auto';
+import type {
+    UsbDeviceWrapper,
+    UsbDeviceWrapperEndpointStartStopPollFunction,
+    UsbDeviceWrapperEndpointTransferOutFunction,
+} from './UsbDeviceWrapper.js';
+
+export class UsbTransport extends Transport {
+    private transferOut: UsbDeviceWrapperEndpointTransferOutFunction;
+    private startPoll: UsbDeviceWrapperEndpointStartStopPollFunction;
+    private stopPoll: UsbDeviceWrapperEndpointStartStopPollFunction;
+    private onDataBound: (buffer: Uint8Array) => void;
+    private onErrorBound: (error: Error) => void;
+
+    public constructor(
+        private device: UsbDeviceWrapper,
+        events: TransportEvents,
+    ) {
+        super(events);
+
+        [this.transferOut, this.startPoll, this.stopPoll] =
+            this.device.getInterfaceTransferFunctions(0);
+
+        this.onDataBound = this.onData.bind(this);
+        this.onErrorBound = this.onError.bind(this);
+    }
+
+    private onError(err: Error): void {
+        this.events.onError(err);
+        this.events.onDisconnected();
+    }
+
+    private onData(buffer: Uint8Array): void {
+        this.events.onData(buffer);
+    }
+
+    public async connect(): Promise<void> {
+        if (this.state !== TransportState.AVAILABLE) {
+            return;
+        }
+
+        await this.device.claimInterface(0);
+
+        this.startPoll(this.onDataBound, this.onErrorBound);
+
+        this.state = TransportState.CONNECTED;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/require-await
+    public async disconnect(): Promise<void> {
+        if (this.state !== TransportState.CONNECTED) {
+            return;
+        }
+
+        this.state = TransportState.DISCONNECTED;
+
+        this.stopPoll(this.onDataBound, this.onErrorBound);
+
+        await this.device.releaseInterface(0);
+    }
+
+    public async send(buffer: Uint8Array): Promise<void> {
+        if (this.state === TransportState.DISCONNECTED) {
+            throw new Error('Cannot send to disconnected tranport');
+        }
+
+        return this.transferOut(buffer);
+    }
+}
